be5dbeb1a9d0e7da13de50d8a21928176f31728d
Damian Jasiński <dam03jas@gmail.com>
2024-12-16 14:53:13 +0100

    wpisywanie liczb i sprawdzanie czy jest wieksza od zera

--- zad5.cpp
+++ zad5.cpp
@@ -19,3 +19,23 @@ double calculate_partial_integral(double start, double end, unsigned long long s
     return sum * step_size;
 }
 
+int main() {
+    unsigned long long num_steps;
+    int num_threads;
+
+    // User input
+    std::cout << "Enter the number of steps (e.g., 1000000000): ";
+    std::cin >> num_steps;
+    std::cout << "Enter the number of threads: ";
+    std::cin >> num_threads;
+
+    // Validate input
+    if (num_threads <= 0 || num_steps <= 0) {
+        std::cerr << "Number of threads and steps must be positive!" << std::endl;
+        return 1;
+    }
+
+
+
+    return 0;
+}
